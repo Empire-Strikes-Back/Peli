d35bf9f25129e65d263926bf1c52bd2a54644905
Peli <Peli@spaceport>
2022-03-22 17:32:00 +0300

    :Martin-Freeman stop dropping me on my head!

--- bounty_puck.sh
+++ bounty_puck.sh
@@ -4,33 +4,7 @@ repl(){
   clj \
     -J-Dclojure.core.async.pool-size=1 \
     -X:repl:$1 Ripley.core/process \
-    :main-ns Peli.${1}-main
-}
-
-main(){
-  clojure \
-    -J-Dclojure.core.async.pool-size=1 \
-    -M:$1 -m Peli.${1}-main
-}
-
-jar(){
-
-  clojure \
-    -X:identicon Zazu.core/process \
-    :word "\"$1\"" \
-    :filename '"out/identicon/icon.png"' \
-    :size 256
-
-  rm -rf out/*.jar
-  clojure \
-    -X:uberjar Genie.core/process \
-    :main-ns Peli.${1}-main \
-    :filename "\"out/Peli-$1-$(git rev-parse --short HEAD).jar\"" \
-    :paths '["src" "out/identicon"]'
-}
-
-release(){
-  jar
+    :main-ns bounty-puck.${1}-main
 }
 
 "$@"
--- src/Peli/microwaved_potatoes_main.clj
+++ src/Peli/microwaved_potatoes_main.clj
@@ -1,5 +1,4 @@
-(ns Peli.baked-potatoes-main
-  "https://www.kaggle.com/c/mens-march-mania-2022"
+(ns Peli.microwaved-potatoes-main
   (:require
    [clojure.core.async :as Little-Rock
     :refer [chan take! put! offer! close! to-chan! timeout thread
@@ -9,28 +8,18 @@
             pipe pipeline pipeline-async]]
    [clojure.string :as Wichita.string]
    [clojure.java.io :as Wichita.io]
-   [clojure.repl :as Wichita.repl]
-   [Ripley.core])
-  (:gen-class))
-
+   [clojure.repl :as Wichita.repl]))
 
 
 (defn reload
   []
-  (require '[Peli.baked-potatoes-main] :reload))
+  (require '[Peli.microwaved-potatoes-main] :reload))
 
 (defn process
-  [])
-
-(defn proclaim
   []
-  (let [fn-names (keys (ns-publics 'Peli.baked-potatoes-main))]
-    (doseq [fn-name fn-names]
-      (println (eval `(with-out-str (clojure.repl/doc ~fn-name))))))
   (println "Kuiil has spoken"))
 
 (defn -main
   [& main]
   (println "i dont want my next job")
-  (proclaim)
-  (Ripley.core/process {:main-ns 'Peli.baked-potatoes-main}))
+  (process))
